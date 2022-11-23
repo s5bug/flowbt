24d8bb87c4e874889838250480d3809c0bf9b6d1
Aly <me@s5.pm>
2022-11-23 00:05:20 -0800

    Fix logging statements being discarded

--- build.sbt
+++ build.sbt
@@ -19,13 +19,14 @@ lazy val cli = (project in file("cli")).settings(
   scalaVersion := "2.13.10",
   libraryDependencies ++= Seq(
     "org.typelevel" %% "cats-core" % "2.9.0",
-    "org.typelevel" %% "cats-effect" % "3.4.0",
+    "org.typelevel" %% "cats-effect" % "3.4.1",
     "co.fs2" %% "fs2-core" % "3.3.0",
-    "com.outr" %% "scribe" % "3.10.4",
-    "com.outr" %% "scribe-cats" % "3.10.4",
+    "com.outr" %% "scribe" % "3.10.5",
+    "com.outr" %% "scribe-cats" % "3.10.5",
     "org.gnieh" %% "fs2-data-xml" % "1.6.0",
     "com.monovore" %% "decline" % "2.3.1",
     "com.monovore" %% "decline-effect" % "2.3.1",
   ),
   Compile / mainClass := Some("tf.bug.flowbt.cli.Main"),
+  fork := true,
 )
--- cli/src/main/scala/tf/bug/flowbt/cli/Main.scala
+++ cli/src/main/scala/tf/bug/flowbt/cli/Main.scala
@@ -2,51 +2,68 @@ package tf.bug.flowbt.cli
 
 import cats.effect._
 import cats.syntax.all._
-import com.monovore.decline.Opts
-import com.monovore.decline.effect.CommandIOApp
+import com.monovore.decline.{Command, Opts}
 import java.nio.file.{Path, Paths}
 import scribe.{Level, Logger, Scribe}
 import scribe.cats._
 
-object Main extends CommandIOApp("flowbt", "Flowgram Build Tool") {
+object Main extends IOApp {
 
-  override def main: Opts[IO[ExitCode]] = {
+  case class Arguments(
+    buildSheet: Path,
+    generatedFolder: Path,
+    verbose: Boolean
+  )
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    val cmd = Command("flowbt", "Flowgram Build Tool", helpFlag = true)(opts)
+    cmd.parse(args) match {
+      case Left(help) => std.Console[IO].errorln(help).as(ExitCode.Error)
+      case Right(args) =>
+        val level = if(args.verbose) Level.Trace else Level.Info
+        val replaceLogger = IO {
+          Logger.root
+            .clearModifiers()
+            .clearHandlers()
+            .withHandler(minimumLevel = Some(level))
+            .replace()
+        }
+        replaceLogger *> program[IO](args.buildSheet, args.generatedFolder)
+    }
+  }
+
+  def opts: Opts[Arguments] = {
     val buildSheet = Opts.argument[Path]("build sheet")
     val generatedFolder = Opts.argument[Path]("generated folder")
-    val verbose = Opts.flag("verbose", "Enable verbose logging", "v").orFalse
-    (buildSheet, generatedFolder, verbose).mapN { (bs, gf, v) =>
-      val level = if(v) Level.Trace else Level.Info
-      IO {
-        Logger.root
-          .clearModifiers()
-          .clearHandlers()
-          .withHandler(minimumLevel = Some(level))
-          .replace()
-      } *> program[IO](bs, gf)
-    }
+    val verbose = Opts.flag("verbose", "Enable verbose logging.", "v").orFalse
+    (buildSheet, generatedFolder, verbose).mapN(Arguments)
   }
 
   def program[F[_]](buildSheet: Path, generatedFolder: Path)(implicit sync: Sync[F], scribe: Scribe[F]): F[ExitCode] = for {
     _ <- scribe.trace(s"ENTER program(buildSheet = $buildSheet, generatedFolder = $generatedFolder)")
-    ec <- sync.delay(Paths.get(".")).map(_.toAbsolutePath).flatMap { absoluteCwd =>
-      val absoluteBuildSheet = buildSheet.toAbsolutePath
+    ec <- sync.delay(Paths.get(".")).map(_.toAbsolutePath.normalize()).flatMap { absoluteCwd =>
+      val absoluteBuildSheet = buildSheet.toAbsolutePath.normalize()
       val buildSheetHere = absoluteBuildSheet.startsWith(absoluteCwd)
-      val absoluteGeneratedFolder = generatedFolder.toAbsolutePath
+      val absoluteGeneratedFolder = generatedFolder.toAbsolutePath.normalize()
       val generatedFolderHere = absoluteGeneratedFolder.startsWith(absoluteCwd)
 
-      scribe.debug(s"Starting flowbt")
-      scribe.debug(s"CWD: $absoluteCwd")
-      scribe.debug(s"Build Sheet: $absoluteBuildSheet")
-      scribe.debug(s"Generated Folder: $absoluteGeneratedFolder")
+      val contextLogs: F[Unit] = List(
+        s"Starting flowbt",
+        s"CWD: $absoluteCwd",
+        s"Build Sheet: $absoluteBuildSheet",
+        s"Generated Folder: $absoluteGeneratedFolder"
+      ).traverse_(scribe.debug(_))
 
-      if (buildSheetHere && generatedFolderHere) {
-        sync.pure(ExitCode.Success)
-      } else {
-        val errorStrings = List(
-          Option.when(!buildSheetHere)("Build sheet not beneath current working directory."),
-          Option.when(!generatedFolderHere)("Generated folder not beneath current working directory.")
-        ).flatten
-        errorStrings.traverse_(scribe.error(_)).as(ExitCode.Error)
+      contextLogs.flatMap { _ =>
+        if (buildSheetHere && generatedFolderHere) {
+          sync.pure(ExitCode.Success)
+        } else {
+          val errorStrings = List(
+            Option.when(!buildSheetHere)("Build sheet not beneath current working directory."),
+            Option.when(!generatedFolderHere)("Generated folder not beneath current working directory.")
+          ).flatten
+          errorStrings.traverse_(scribe.error(_)).as(ExitCode.Error)
+        }
       }
     }
     _ <- scribe.trace(s"LEAVE program(buildSheet = $buildSheet, generatedFolder = $generatedFolder)")
